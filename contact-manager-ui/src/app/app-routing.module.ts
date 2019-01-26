daa1869068ec3bc1742ffe7e0063b454e6dbafa9
ABHIJITH S J <abhijith.sj@oracle.com>
2019-01-26 19:31:58 +0530

    Initial Check -in for First Data Layer and UI Layer of Content-Manager Angular Project with cli -tool

---
+++ contact-manager-ui/src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'contacts',
+    pathMatch: 'full'
+  },
+  {
+    path: 'contacts',
+    component: ContactListComponent
+  },
+  {
+    path: 'new',
+    component: AddContactComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'contacts'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
